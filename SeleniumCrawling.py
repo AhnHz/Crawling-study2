de2ebf7c74954df2bcef19f1a67752b3cf405749
AhnHz <132975657+AhnHz@users.noreply.github.com>
2023-08-18 01:20:50 +0900

    Add files via upload

---
+++ SeleniumCrawling.py
@@ -0,0 +1,29 @@
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.action_chains import ActionChains
+import time
+
+driver = webdriver.Chrome()
+# driver.get("https://www.opinet.co.kr/searRgSelect.do")
+driver.get("https://www.opinet.co.kr/user/main/mainView.do")
+time.sleep(2)
+test = driver.find_elements(By.CLASS_NAME, "gnbTopa")[0]
+ActionChains(driver).move_to_element(test).perform()
+
+time.sleep(2)
+
+region = driver.find_element(By.ID, "SIDO_NM0")
+region_detail = region.find_elements(By.TAG_NAME, "option")
+for i in region_detail:
+    print(i.text)
+
+region_detail[2].click()
+time.sleep(2)
+
+region_second = driver.find_element(By.NAME, "SIGUNGU_NM0")
+region_second_detail = region_second.find_elements(By.TAG_NAME, "option")[1:]
+for i in region_second_detail:
+    print(i.text)
+
+driver.close()
